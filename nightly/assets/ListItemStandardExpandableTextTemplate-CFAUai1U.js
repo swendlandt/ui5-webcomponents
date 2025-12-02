d1522a41d2afb4c8aa32685aa21dbfb584a57c7b
swendlandt <swendlandt@users.noreply.github.com>
2025-12-02 01:06:17 +0000

    Deploying to gh-pages from @ swendlandt/ui5-webcomponents@d1c69174bd0a45eb4d29cfd14c16cb493dd3df87 🚀

--- nightly/assets/ListItemStandardExpandableTextTemplate-CFAUai1U.js
+++
@@ -1,1 +0,0 @@
-import{a as x,E as m}from"./bundle.esm-igU0ThbT.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.1",
     isNext: false,
-    buildTime: 1764551561,
+    buildTime: 1764637316,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
