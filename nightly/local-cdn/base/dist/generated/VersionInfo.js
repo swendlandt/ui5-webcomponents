cad1e226e523377e12d35400713c2050517ad0e5
swendlandt <swendlandt@users.noreply.github.com>
2025-12-12 01:08:24 +0000

    Deploying to gh-pages from @ swendlandt/ui5-webcomponents@d1c69174bd0a45eb4d29cfd14c16cb493dd3df87 🚀

--- nightly/local-cdn/base/dist/generated/VersionInfo.js
+++ nightly/local-cdn/base/dist/generated/VersionInfo.js
@@ -5,7 +5,7 @@ const VersionInfo = {
     patch: 0,
     suffix: "-rc.1",
     isNext: false,
-    buildTime: 1765415062,
+    buildTime: 1765501439,
 };
 export default VersionInfo;
 //# sourceMappingURL=VersionInfo.js.map
