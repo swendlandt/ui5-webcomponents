3e52ddc7dc0e05a8f2f003bc22a4e7210369bdc8
swendlandt <swendlandt@users.noreply.github.com>
2025-12-31 01:10:09 +0000

    Deploying to gh-pages from @ swendlandt/ui5-webcomponents@d1c69174bd0a45eb4d29cfd14c16cb493dd3df87 🚀

--- nightly/local-cdn/main/dist/assets/ListItemStandardExpandableTextTemplate-ByCFOY83.js
+++
@@ -1,1 +0,0 @@
-import{a as x,E as m}from"./bundle.esm-DhuT_Ywo.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
--- nightly/local-cdn/main/dist/assets/test/pages/Icon.html-ys8N5E_D.js
+++
@@ -1,1 +0,0 @@
-import"../../bundle.esm-DhuT_Ywo.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
