e213074226ae43f3b225a8a6326248a13cdbe192
swendlandt <swendlandt@users.noreply.github.com>
2026-01-09 01:10:59 +0000

    Deploying to gh-pages from @ swendlandt/ui5-webcomponents@d1c69174bd0a45eb4d29cfd14c16cb493dd3df87 🚀

--- nightly/local-cdn/main/dist/assets/ListItemStandardExpandableTextTemplate-C6acrrLI.js
+++
@@ -1,1 +0,0 @@
-import{a as x,E as m}from"./bundle.esm-BXq9otEL.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
