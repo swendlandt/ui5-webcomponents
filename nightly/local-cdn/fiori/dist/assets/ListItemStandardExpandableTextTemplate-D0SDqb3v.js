fe2f895a45b9ffcd896da20f1ad32b807bde0666
swendlandt <swendlandt@users.noreply.github.com>
2026-02-14 01:21:51 +0000

    Deploying to gh-pages from @ swendlandt/ui5-webcomponents@d1c69174bd0a45eb4d29cfd14c16cb493dd3df87 🚀

--- nightly/local-cdn/fiori/dist/assets/ListItemStandardExpandableTextTemplate-D0SDqb3v.js
+++
@@ -1,1 +0,0 @@
-import{j as x,aw as m}from"./bundle.esm-rqqgA_qq.js";function p(a){const{className:t,text:e,maxCharacters:r,part:s}=a;return x(m,{part:s,class:t,text:e,maxCharacters:r})}export{p as default};
