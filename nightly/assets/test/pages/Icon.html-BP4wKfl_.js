67606d33e0d38b7c36bfbd4a96102ea941c5d899
swendlandt <swendlandt@users.noreply.github.com>
2026-09-23 02:37:58 +0000

    Deploying to gh-pages from @ swendlandt/ui5-webcomponents@d1c69174bd0a45eb4d29cfd14c16cb493dd3df87 🚀

---
+++ nightly/assets/test/pages/Icon.html-BP4wKfl_.js
@@ -0,0 +1,1 @@
+import"../../bundle.esm-DutD4Ula.js";(async()=>{var a=await window["sap-ui-webcomponents-bundle"].getIconNames();a.forEach(e=>{var n=document.createElement("ui5-icon");n.name=e,allIcons.appendChild(n)})})();
